981da1baa96059504851885a556a0819ef8a60a7
Amanda <aleisero@ucsc.edu>
2019-02-16 16:06:27 -0800

    animated pick up item that unlocks animated door implemented

---
+++ Assets/collectableScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class collectableScript : MonoBehaviour
+{
+    //public bool isGot = false;
+
+    public GameObject door;
+
+    private int delay = 0;
+    private bool okPickUp = true;
+    
+
+    private void Update()
+    {
+        delay++;
+        if (delay == 15)
+        {
+            okPickUp = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (okPickUp)
+        {
+            okPickUp = false;
+            delay = 0;
+
+            //isGot = true;
+            door.GetComponent<doorMove>().Unlock();
+
+            gameObject.SetActive(false);   
+        }
+    }
+}
---
+++ Assets/doorMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorMove : MonoBehaviour
+{
+    private Animator doorMover;
+    
+
+    void Start()
+    {
+        doorMover = this.GetComponent<Animator>();
+    }
+    
+    public void Unlock()
+    {
+        doorMover.SetBool("keyGot", true);
+    }
+}
